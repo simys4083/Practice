4bc34b06f7fe49c2a97c7922de78f6264ff139e9
[Github <[Github.simys4084@gmail.com]>
2023-02-19 23:07:49 +0900

    first commit

---
+++ MyNewScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyNewScript : MonoBehaviour 
+{
+	public string PlayerName = "";
+	public int PlayerHealth = 100;
+	public Vector3 Position = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(PlayerName);
+        Debug.Log(PlayerHealth);
+        Debug.Log("Position : " + transform.position);
+    }
+	// Update is called once per frame
+	void Update () {}
+}
